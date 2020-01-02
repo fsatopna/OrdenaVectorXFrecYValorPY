5bb67151bb5dc8008f38881fec4fe54898a2c896
fsatopna <42137693+fsatopna@users.noreply.github.com>
2020-01-02 13:02:33 -0300

    Add files via upload

---
+++ ordenarArrayFrecuenciaYValor.py
@@ -0,0 +1,46 @@
+arr =  [5,1,5,4,3,5,5,3]
+print("entrada         : ",arr)
+
+#arr.sort()
+#print("entrada ordenada: ",arr)
+
+anum = list(set(arr))
+print("col1: set nros  : ",anum)
+
+afrec = []
+for n in anum:
+    afrec.append(arr.count(n))
+    
+print("col2: set frecs : ",afrec)    
+
+anf= list(zip(afrec,anum))
+print("pares (frec,num): ",anf)    
+
+anfs = sorted(anf)
+#print("pares ordenados : ",anfs)
+
+res =  []
+for fila in anfs:
+#    print(fila[1],'->',fila[0])
+    for i in range(fila[0]):
+        res.append(fila[1])
+        
+print("resultado       : ",res)
+
+'''
+ def cmp_tuplas(a, b):
+   if a[0]>b[0]:
+      return 1
+   elif a[0]<b[0]:
+      return -1
+   else:
+       if a[1]<b[1]:
+          return 1
+       elif a[1]>b[1]:
+          return -1
+       else:
+          return 0
+   
+sorted(anf, cmp=cmp_tuplas)  # Python2 solo!
+     
+'''
